4a8f587720743e876aca00776550a4e5db9cc338
kar-tix <karolina_barylak01@spoko.pl>
2026-04-12 22:11:56 +0200

    main

---
+++ #1 - Danny's Diner/README.md
@@ -0,0 +1,3 @@
+# <center>Case Study #1 - Danny's Diner</center>
+
+![Logo case study #!](img/logo_case.png)
---
+++ #1 - Danny's Diner/solutions/answer-bonus-2.sql
@@ -0,0 +1,30 @@
+-- Rank All The Things
+-- Uporzadkuj Wszystkie Rzeczy
+
+WITH all_joined AS (
+    SELECT
+        sales.customer_id,
+        sales.order_date,
+        menu.product_name,
+        menu.price,
+        CASE
+            WHEN sales.order_date >= members.join_date THEN 'Y'
+            WHEN sales.order_date < members.join_date THEN 'N'
+            ELSE 'N'
+        END as members
+    FROM sales
+    INNER JOIN menu
+        ON sales.product_id = menu.product_id
+    LEFT JOIN members
+        ON sales.customer_id = members.customer_id
+    ORDER BY sales.customer_id, sales.order_date, menu.price DESC
+)
+
+SELECT *,
+    CASE
+        WHEN members = 'N' THEN NULL
+        ELSE DENSE_RANK() OVER
+            (PARTITION BY customer_id, members
+            ORDER BY order_date)
+    END as ranking
+FROM all_joined
